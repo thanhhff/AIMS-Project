483cf383e1bd78c1d8bb4439f59bb02f2d3eae32
baoanh1310 <55654762+baoanh1310@users.noreply.github.com>
2020-12-18 16:41:20 +0700

    Add value property for Media model

--- src/model/Media/Media.java
+++ src/model/Media/Media.java
@@ -13,21 +13,19 @@ public class Media {
     private int media_id;
     private String title;
     private int price;
+    private int value;
 
     public Media() {
         title = "";
+        value = 0;
         price = 0;
     }
     
-    public Media(int media_id, String title, int price) {
+    public Media(int value, String title, int price) {
         this.media_id = media_id;
         this.title = title;
-        this.price = price;
-    }
-    
-    public Media(String title, int price) {
-        this.title = title;
-        this.price = price;
+        this.value = value;
+        setPrice(price);
     }
     
     public int getMediaId() {
@@ -38,15 +36,32 @@ public class Media {
         return price;
     }
     
+    public int getValue() {
+        return value;
+    }
+    
     public String getTitle() {
         return title;
     }
     
     public void setPrice(int price) {
-        this.price = price;
+        int value = getValue();
+        int min = (int)(value * 0.3);
+        int max = (int)(value * 1.5);
+        if (price < min) {
+            this.price = min;
+        } else if (price > max) {
+            this.price = max;
+        } else {
+            this.price = price;
+        }
     }
     
     public void setTitle(String title) {
         this.title = title;
+    }
+    
+    public void setValue(int value) {
+        this.value = value;
     }
 }
