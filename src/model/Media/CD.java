6ded0d8399d967f42a5f771ff47cc7c81369e425
baoanh1310 <55654762+baoanh1310@users.noreply.github.com>
2020-12-19 11:00:51 +0700

    Add CD model

---
+++ src/model/Media/CD.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.Media;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Toshiba T75
+ */
+public class CD extends PhysicalGood {
+    private String artist_name;
+    private String record_label_name;
+    private String publication_date;
+    private String genre;
+    private ArrayList<Track> track_list;
+    
+    public CD(String title, int value, int price, int sale_percent, String category, String barcode, String description, int quantity, String date, int dimension_x, int dimension_y, int dimension_z, int weight, String artist, String record_label, String publication_date, String genre) {
+        super(title, value, price, sale_percent, category, barcode, description, quantity, date, dimension_x, dimension_y, dimension_z, weight);
+        this.artist_name = artist;
+        this.record_label_name = record_label;
+        this.publication_date = publication_date;
+        this.genre = genre;
+        this.track_list =  new ArrayList<>();
+    }
+    
+    public String getArtistName() {
+        return artist_name;
+    }
+    
+    public void setArtistName(String artist) {
+        this.artist_name = artist;
+    }
+    
+    public String getRecordLabelName() {
+        return record_label_name;
+    }
+    
+    public void setRecordLabelName(String record) {
+        this.record_label_name = record;
+    }
+    
+    public String getPublicationDate() {
+        return publication_date;
+    }
+    
+    public void setPublicationDate(String date) {
+        this.publication_date = date;
+    }
+    
+    public String getGenre() {
+        return genre;
+    }
+    
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    public ArrayList<Track> getTrackList() {
+        return track_list;
+    }
+    
+    public void setTrackList(ArrayList<Track> track_list) {
+        this.track_list = track_list;
+    }
+    
+    public void addTrack(Track track) {
+        this.track_list.add(track);
+    }
+    
+    public void removeTrackList() {
+        this.track_list = new ArrayList<>();
+    }
+}
