881ef2243b63ef1f02e6d5c159c28694cfedda95
HSNT <hieu.jno1@gmail.com>
2020-12-19 22:36:21 +0700

    Add event Cart null

--- src/views/HomeUser/HomeUser.java
+++ src/views/HomeUser/HomeUser.java
@@ -10,6 +10,7 @@ import aims.PaymentService;
 import controller.Cart.CartController;
 import java.awt.BorderLayout;
 import java.awt.LayoutManager;
+import javax.swing.JOptionPane;
 import model.User.User;
 import views.MediaList.mediaList;
 import views.account.UserPanel.AccountPanel;
@@ -29,9 +30,8 @@ public final class HomeUser extends javax.swing.JFrame {
         setLayout(null);
         fill();
     }
-    
-    public void fill()
-    {
+
+    public void fill() {
         mediaList media = new mediaList();
         jpLayout.setLayout(new BorderLayout());
         jpLayout.add(media, BorderLayout.CENTER);
@@ -60,12 +60,12 @@ public final class HomeUser extends javax.swing.JFrame {
         javax.swing.GroupLayout jpLayoutLayout = new javax.swing.GroupLayout(jpLayout);
         jpLayout.setLayout(jpLayoutLayout);
         jpLayoutLayout.setHorizontalGroup(
-            jpLayoutLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+                jpLayoutLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 0, Short.MAX_VALUE)
         );
         jpLayoutLayout.setVerticalGroup(
-            jpLayoutLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 698, Short.MAX_VALUE)
+                jpLayoutLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 698, Short.MAX_VALUE)
         );
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/views/images/magnifying-glass.png"))); // NOI18N
@@ -96,79 +96,81 @@ public final class HomeUser extends javax.swing.JFrame {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addComponent(Home)
-                .addGap(36, 36, 36)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 593, Short.MAX_VALUE)
-                .addComponent(Cart, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(Account)
-                .addGap(40, 40, 40))
-            .addComponent(jpLayout, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jSeparator2)
-                .addContainerGap())
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(14, 14, 14)
+                                .addComponent(Home)
+                                .addGap(36, 36, 36)
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 593, Short.MAX_VALUE)
+                                .addComponent(Cart, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(Account)
+                                .addGap(40, 40, 40))
+                        .addComponent(jpLayout, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jSeparator2)
+                                .addContainerGap())
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(7, 7, 7)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(Account, javax.swing.GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                            .addGap(8, 8, 8)
-                            .addComponent(jLabel2))
-                        .addComponent(Cart, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(Home)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
-                .addComponent(jpLayout, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(7, 7, 7)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                                .addComponent(Account, javax.swing.GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)
+                                                .addGroup(layout.createSequentialGroup()
+                                                        .addGap(8, 8, 8)
+                                                        .addComponent(jLabel2))
+                                                .addComponent(Cart, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                        .addComponent(Home)
+                                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
+                                .addComponent(jpLayout, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
     }// </editor-fold>                        
 
-    private void AccountMouseClicked(java.awt.event.MouseEvent evt) {                                     
+    private void AccountMouseClicked(java.awt.event.MouseEvent evt) {
         // TODO add your handling code here:
         jpLayout.removeAll();
         AccountPanel accPanel = new AccountPanel();
         jpLayout.setLayout(new BorderLayout());
         jpLayout.add(accPanel, BorderLayout.CENTER);
         jpLayout.updateUI();
-    }                                    
+    }
 
-    private void HomeMouseClicked(java.awt.event.MouseEvent evt) {                                  
+    private void HomeMouseClicked(java.awt.event.MouseEvent evt) {
         // TODO add your handling code here:
         jpLayout.removeAll();
         mediaList media = new mediaList();
         jpLayout.setLayout(new BorderLayout());
         jpLayout.add(media, BorderLayout.CENTER);
         jpLayout.updateUI();
-    }                                 
+    }
 
-    private void CartMouseClicked(java.awt.event.MouseEvent evt) {                                  
+    private void CartMouseClicked(java.awt.event.MouseEvent evt) {
         // TODO add your handling code here:
         User user = new User();
-        
-        CartPanel cart = new CartPanel(user, new CartController(new PaymentService()));
-        
-        jpLayout.removeAll();
-        jpLayout.setLayout(new BorderLayout());
-        cart.setBounds(0,0, jpLayout.getWidth(), jpLayout.getHeight());
-        jpLayout.add(cart);
-        jpLayout.updateUI();
+        if (user.getCartItems() == null) {
+            JOptionPane.showMessageDialog(null, "Cart empty");
+        } else {
+            CartPanel cart = new CartPanel(user, new CartController(new PaymentService()));
 
-    }                                 
+            jpLayout.removeAll();
+            jpLayout.setLayout(new BorderLayout());
+            cart.setBounds(0, 0, jpLayout.getWidth(), jpLayout.getHeight());
+            jpLayout.add(cart);
+            jpLayout.updateUI();
+        }
 
+    }
 
     // Variables declaration - do not modify                     
     private javax.swing.JLabel Account;
