34659620887fe38e0721abd74eee602cf7d45183
baoanh1310 <55654762+baoanh1310@users.noreply.github.com>
2020-12-25 12:48:58 +0700

    Add get controller methods for Track

--- src/model/Media/Track.java
+++ src/model/Media/Track.java
@@ -5,6 +5,10 @@
  */
 package model.Media;
 
+import db.ConnectSQL;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 /**
  *
  * @author Toshiba T75
@@ -36,5 +40,49 @@ public class Track {
     
     public void setTrackName(String track_name) {
         this.track_name = track_name;
+    }
+    
+    public static ArrayList<Track> getAllTrack() {
+        ArrayList<Track> tracks = new ArrayList<Track>();
+        String select_query = "SELECT * FROM tracks;";
+        try {
+            ResultSet rs = ConnectSQL.sqlQuery(select_query);
+            while (rs.next()) {
+                int track_id = rs.getInt(1);
+                String track_name = rs.getString(2);
+                Track track = new Track(track_id, track_name);
+                tracks.add(track);
+            }
+            
+            return tracks;
+        } catch (Exception e) {
+            return tracks;
+        }
+    }
+    
+    public static String getTrackNameFromDB(int track_id) {
+        String select_query = "SELECT track_name FROM tracks WHERE track_id = " + track_id + ";";
+        try {
+            ResultSet rs = ConnectSQL.sqlQuery(select_query);
+            String track_name = "";
+            if (rs.next()) {
+                track_name = rs.getString(1);
+            }
+            return track_name;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public static boolean checkDuplicateTrackName(String track_name) {
+        ArrayList<Track> tracks = Track.getAllTrack();
+        boolean result = false;
+        for (Track track: tracks) {
+            if (track.getTrackName() == track_name) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
