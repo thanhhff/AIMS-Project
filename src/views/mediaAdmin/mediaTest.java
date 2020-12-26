d7242fb024b5fc92ca73730811e5e4269f985269
baoanh1310 <55654762+baoanh1310@users.noreply.github.com>
2020-12-26 13:48:54 +0700

    Test get all physicalgood medias from DB successfully

--- src/views/mediaAdmin/mediaTest.java
+++ src/views/mediaAdmin/mediaTest.java
@@ -14,10 +14,33 @@ import java.util.ArrayList;
  */
 public class mediaTest {
     public static void main(String[] args)  {
-        String track_list = "Chung ta cua hien tai,Chay ngay di";
-        String[] splitArr = track_list.split(",");
-        for (String s: splitArr) {
-            System.out.println(s);
+        ArrayList<Media>medias = Media.getAllMedia();
+        System.out.println(medias.size());
+        int book_cnt = 0;
+        int dvd_cnt = 0;
+        int cd_cnt = 0;
+        int lp_cnt = 0;
+        for (Media media: medias) {
+            System.out.println(media.getTitle());
+            int category_id = media.getCategoryId();
+            switch (category_id) {
+                case 1:
+                    book_cnt += 1;
+                    break;
+                case 2:
+                    dvd_cnt += 1;
+                    break;
+                case 3:
+                    cd_cnt += 1;
+                    break;
+                case 4:
+                    lp_cnt += 1;
+                    break;
+            }
         }
+        System.out.println("Book: " + book_cnt);
+        System.out.println("DVD: " + dvd_cnt);
+        System.out.println("CD: " + cd_cnt);
+        System.out.println("LP: " + lp_cnt);
     }
 }
