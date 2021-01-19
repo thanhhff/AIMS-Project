98f6d4096c99b32174d9286fe2e9e6cc8dba0594
baoanh1310 <55654762+baoanh1310@users.noreply.github.com>
2021-01-19 09:39:54 +0700

    Add new sql in ver3

--- build/classes/db/itss_ver3.sql
+++ build/classes/db/itss_ver3.sql
@@ -12872,21 +12872,24 @@ ALTER TABLE `TrackLists`
 -- Các ràng buộc cho bảng `Wards`
 --
 
+
+CREATE TABLE `ActionsHistoryNew` (
+  action_id int NOT NULL,
+  user_id int NOT NULL,
+  date datetime NOT NULL,
+  media_id bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+ALTER TABLE `ActionsHistoryNew`
+ADD CONSTRAINT `fk_ActionHistoryNew_Actions` FOREIGN KEY (action_id) REFERENCES Actions(action_id),
+ADD CONSTRAINT `fk_ActionHistoryNew_Users` FOREIGN KEY (user_id) REFERENCES Users(user_id),
+ADD CONSTRAINT `fk_ActionHistoryNew_Medias` FOREIGN KEY (media_id) REFERENCES Medias(media_id);
+
 ALTER TABLE `Wards`
   ADD CONSTRAINT `fk_SubDistricts_Districts1` FOREIGN KEY (`district_id`) REFERENCES `Districts` (`district_id`);
 COMMIT;
 
-CREATE TABLE `ActionsHistoryNew` (
-  `action_id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `date` datetime NOT NULL,
-  `media_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-ALTER TABLE `ActionsHistoryNew` 
-    ADD CONSTRAINT `fk_actionshistorynew_actions` FOREIGN KEY (`action_id`) REFERENCES `Actions`(`action_id`) ON DELETE CASCADE,
-    ADD CONSTRAINT `fk_actionshistorynew_users` FOREIGN_KEY (`user_id`) REFERENCES `Users` (`user_id`) ON DELETE CASCADE,
-    ADD CONSTRAINT `fk_actionshistorynew_medias` FOREIGN KEY (`media_id`) REFERENCES `Medias` (`media_id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
